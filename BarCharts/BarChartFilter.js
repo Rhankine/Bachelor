34cf1a0df7c0a9a3dcd657186c13b1fa31ed6522
Bocar <bo.carlsen@hotmail.dk>
2016-05-24 00:19:39 +0200

    Ranges is now filtered as well.

--- BarCharts/BarChartFilter.js
+++ BarCharts/BarChartFilter.js
@@ -1,7 +1,7 @@
 var order = ["Group", "HR", "Procurement", "Legal", "Shipping", "Sales"];
 
 var margin = {top: 20, right: 20, bottom: 30, left: 40},
-    width = 500 - margin.left - margin.right,
+    width = order.length * 80 - margin.left - margin.right,
     height = 500 - margin.top - margin.bottom;
     
 var x = d3.scale.ordinal()
@@ -19,85 +19,83 @@ var yAxis = d3.svg.axis()
     .orient("left")
     .ticks(10);
 
+var svg = d3.select("body").append("svg")
+    .attr("id", "mainchart")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+.append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-createBarChart();
+d3.tsv("data.tsv", type, function(error, data) {
+if (error) throw error;
 
-function createBarChart() {
-	d3.select("#mainchart").remove();
-    d3.select("#resetbutton").remove();
+for(var key in data) {
+    var val = data[key].department;
     
-    var svg = d3.select("body").append("svg")
-        .attr("id", "mainchart")
-        .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom)
-    .append("g")
-        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-    d3.tsv("data.tsv", type, function(error, data) {
-    if (error) throw error;
-    
-    for(var key in data) {
-        var val = data[key].department;
-        
-        if(order.indexOf(val)!=-1){
-            data[order.indexOf(val)+10] = data[key];
-            delete data[key];
-        }
-    }
-    for(var key in data) {
-        data[key-10] = data[key];
+    if(order.indexOf(val)!=-1){
+        data[order.indexOf(val)+10] = data[key];
         delete data[key];
     }
-    
-    x.domain(data.map(function(d) { if(d==null){return} return d.department; }));
-    y.domain([0, d3.max(data, function(d) { if(d==null){return} return d.revenue; })]);
-
-    svg.append("g")
-        .attr("class", "x axis")
-        .attr("transform", "translate(0," + height + ")")
-        .call(xAxis);
-
-    svg.append("g")
-        .attr("class", "y axis")
-        .call(yAxis)
-        .append("text")
-        .attr("transform", "rotate(-90)")
-        .attr("y", 6)
-        .attr("dy", ".71em")
-        .style("text-anchor", "end")
-        .text("Revenue");
-
-    svg.selectAll(".bar")
-        .data(data)
-        .enter().append("rect")
-        .attr("class", "bar")
-        .attr("id", function(d) {if(d==null){return} return d.department; })
-        .on("click", function(d) {
-            removeBar(d.department);
-            })
-        .attr("x", function(d) {if(d==null){return} return x(d.department); })
-        .attr("width", x.rangeBand())
-        .attr("y", function(d) {if(d==null){return} return y(d.revenue); })
-        .attr("height", function(d) {if(d==null){return} return height - y(d.revenue); });
-    });
-    
-    d3.select("body").append("button")
-        .attr("id", "resetbutton")
-        .text("Reset")
-        .on("click", function(){resetChart(); });
 }
+for(var key in data) {
+    data[key-10] = data[key];
+    delete data[key];
+}
+
+x.domain(order/*data.map(function(d) { if(d==null){return} return d.department; })*/);
+y.domain([0, d3.max(data, function(d) { if(d==null){return} return d.revenue; })]);
+
+svg.append("g")
+    .attr("class", "x axis")
+    .attr("id", "xaxis")
+    .attr("transform", "translate(0," + height + ")")
+    .call(xAxis);
+
+svg.append("g")
+    .attr("class", "y axis")
+    .call(yAxis)
+    .append("text")
+    .attr("transform", "rotate(-90)")
+    .attr("y", 6)
+    .attr("dy", ".71em")
+    .style("text-anchor", "end")
+    .text("Revenue");
+
+svg.selectAll(".bar")
+    .data(data)
+    .enter().append("rect")
+    .attr("class", "bar")
+    .attr("id", function(d) {if(d==null){return} return d.department; })
+    .on("click", function(d) {
+        removeBar(d.department);
+        })
+    .attr("x", function(d) {if(d==null){return} return x(d.department); })
+    .attr("width", x.rangeBand())
+    .attr("y", function(d) {if(d==null){return} return y(d.revenue); })
+    .attr("height", function(d) {if(d==null){return} return height - y(d.revenue); });
+});
+
+d3.select("body").append("button")
+    .attr("id", "resetbutton")
+    .text("Reset")
+    .on("click", function(){resetChart(); });
+
 function type(d) {
   d.revenue = +d.revenue;
   return d;
 }
 
 function resetChart() {
-    order = ["PAX", "Com", "Group", "HR", "Procurement", "Legal", "Shipping", "Sales"];
-    createBarChart();
+    order = ["Group", "HR", "Procurement", "Legal", "Shipping", "Sales"];
 }
 
 function removeBar(id){
-    console.log(id);
     d3.select("#"+id).remove();
-    
+	var oId = order.indexOf(id,1);
+    order.splice(oId,1);
+    console.log(order);
+    x.domain(order);
+    svg.attr("width", width + margin.left + margin.right)
+    x.rangeRoundBands([0, width], .1);
+    svg.select("#xaxis").call(xAxis);
 }
