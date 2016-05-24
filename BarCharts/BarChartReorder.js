2e89ee6c3228d940adb454ad238fa18dbcf3355f
Bocar <bo.carlsen@hotmail.dk>
2016-05-24 23:25:31 +0200

    Barchart reordering done.

--- BarCharts/BarChartReorder.js
+++ BarCharts/BarChartReorder.js
@@ -26,37 +26,48 @@ var svg = d3.select("body").append("svg")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
 d3.tsv("data.tsv", type, function(error, data) {
-if (error) throw error;
+    if (error) throw error;
 
-x.domain(order);
-y.domain([0, d3.max(data, function(d) { if(d==null){return} return d.revenue; })]);
+    x.domain(order);
+    y.domain([0, d3.max(data, function(d) { if(d==null){return} return d.revenue; })]);
 
-svg.append("g")
-    .attr("class", "x axis")
-    .attr("id", "xaxis")
-    .attr("transform", "translate(0," + height + ")")
-    .call(xAxis);
+    svg.append("g")
+        .attr("class", "x axis")
+        .attr("id", "xaxis")
+        .attr("transform", "translate(0," + height + ")")
+        .call(xAxis);
 
-svg.append("g")
-    .attr("class", "y axis")
-    .call(yAxis)
-    .append("text")
-    .attr("transform", "rotate(-90)")
-    .attr("y", 6)
-    .attr("dy", ".71em")
-    .style("text-anchor", "end")
-    .text("Revenue");
+    svg.append("g")
+        .attr("class", "y axis")
+        .call(yAxis)
+        .append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 6)
+        .attr("dy", ".71em")
+        .style("text-anchor", "end")
+        .text("Revenue");
 
-svg.selectAll(".bar")
-    .data(data)
-    .enter().append("rect")
-    .attr("class", "bar drag")
-    .attr("id", function(d) {if(d==null){return} return d.department; })
-    .attr("x", function(d) {if(d==null){return} return x(d.department); })
-    .attr("width", x.rangeBand())
-    .attr("y", function(d) {if(d==null){return} return y(d.revenue); })
-    .attr("height", function(d) {if(d==null){return} return height - y(d.revenue); });
+    var drag = d3.behavior.drag();
+
+    svg.selectAll(".bar")
+        .data(data)
+        .enter().append("rect")
+        .attr("class", "bar drag")
+        .attr("id", function(d) {if(d==null){return} return d.department; })
+        .attr("x", function(d) {if(d==null){return} return x(d.department); })
+        .attr("width", x.rangeBand())
+        .attr("y", function(d) {if(d==null){return} return y(d.revenue); })
+        .attr("height", function(d) {if(d==null){return} return height - y(d.revenue); })
+        .call(drag);
+
+    drag.on("drag", function(d) {
+        d3.select("#"+d.department).attr("x", d3.event.x-30);
+        });
+        
+    drag.on("dragend", function() { sort(); });
 });
+
+
 
 d3.select("body").append("button")
     .attr("id", "resetbutton")
@@ -76,6 +87,26 @@ function removeBar(id){
     x.domain(order);
     width = order.length * 80 - margin.left - margin.right;
     x.rangeRoundBands([0, width], .1);
+    svg.select("#xaxis").call(xAxis);
+    
+    for (i = 0; i < order.length; i++){
+        d3.select("#"+order[i]).attr("x", function() {return x(order[i])})
+    .attr("width", x.rangeBand())
+    }
+}
+
+function sort(){
+    sortArray = [];
+    order.forEach(function(dep) {
+        sortArray[d3.select("#" + dep).attr("x")] = dep;
+        sortArraySorted = Object.keys(sortArray).sort(function(a,b){return sortArray[a]-sortArray[b]})
+    });
+    order = [];
+    sortArraySorted.forEach(function(d) {
+        order.push(sortArray[d]);
+    })
+    
+    x.domain(order);
     svg.select("#xaxis").call(xAxis);
     
     for (i = 0; i < order.length; i++){
