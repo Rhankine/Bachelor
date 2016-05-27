f6c4b1fe6ecc05e99ebd8e9c0ccdf2688407b5ae
Bo Carlsen <bocarlsen@hotmail.dk>
2016-05-27 13:17:54 +0200

    Piechart frontpage updated.

--- PieChart/FrontpagePie.php
+++ PieChart/FrontpagePie.php
@@ -16,19 +16,23 @@
 <!DOCTYPE html>
 <html>
 <head>
+    <link rel="stylesheet" type="text/css" href="./PieChart.css">
+    <script src="//d3js.org/d3.v3.min.js" charset="utf-8"></script>
     <title>Page Title</title>
 </head>
 <body>
     <h1>Welcome to this user perception study.</h1>
-    <p>In this study you will meet 15 pages, each with a pie chart and 2 question. 
+    <p>In this study you will meet 15 pages, each with a pie chart and 2 question. It will take you about 10-15 minutes to complete.<br />
         You will be asked to compare 2 of the pies in the pie chart and tell, how big - in percent - the smaller pie is of the bigger.<br />
         You will have the possibility of using some sort of interaction, which will be explained later.
     </p>
     <p>
-        Each of the 15 pages will look similar to the image below.<br />
-        <img src="./pie.jpg" border="5"><br />
+        Each of the 15 pages will have a chart like the one below.<br />
+        <input type='hidden' value='test' id='h_v' class='h_v'>
+        <script src="./PieChart.js"></script>
         Just below the chart, there are two buttons: left and right. These will rotate the pie counterclockwise 
-        and clockwise respectively.<br />
+        and clockwise respectively. You can try this on the chart below before you continue.<br />
+        It is your choice if you want to use the interaction or not.<br /><br />
         Below this, you will see the two questions. <br />
         The first one "Which pie is smaller?". 
         To this question, please click the button you believe is correct.<br />
