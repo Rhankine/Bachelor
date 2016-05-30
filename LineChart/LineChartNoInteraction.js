f263257c8683dc3248cab07f2ec85582697ec176
Bo Carlsen <bocarlsen@hotmail.dk>
2016-05-30 16:34:21 +0200

    Redundant code removed.

--- LineChart/LineChartNoInteraction.js
+++ LineChart/LineChartNoInteraction.js
@@ -19,85 +19,63 @@ var yAxis = d3.svg.axis()
 	.scale(y)
 	.orient("left")
 	.ticks(10);
+ 
+var svg = d3.select("body").append("svg")
+    .attr("id", "mainchart")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+.append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-createLineChart();
+var did = d3.select("#h_v").attr("value");
 
-function createLineChart() {    
-    var svg = d3.select("body").append("svg")
-        .attr("id", "mainchart")
-        .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom)
-    .append("g")
-        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+d3.tsv("./../DataMon/data"+did+".tsv", type, function(error, data) {
+if (error) throw error;
 
-    var did = d3.select("#h_v").attr("value");
+x.domain(data.map(function(d) { if(d==null){return} return d.month; }));
+y.domain([0, d3.max(data, function(d) { if(d==null){return} return d.revenue; })]);
 
-    d3.tsv("./../DataMon/data"+did+".tsv", type, function(error, data) {
-    if (error) throw error;
+svg.append("g")
+    .attr("class", "x axis")
+    .attr("transform", "translate(0," + height + ")")
+    .call(xAxis);
+
+svg.append("g")
+    .attr("class", "y axis")
+    .call(yAxis)
+    .append("text")
+    .attr("transform", "rotate(-90)")
+    .attr("y", 6)
+    .attr("dy", ".71em")
+    .style("text-anchor", "end")
+    .text("Revenue");
     
-    for(var key in data) {
-        var val = data[key].month;
-        
-        if(exclude.indexOf(val)!=-1){
-            delete data[key];
+svg.append("path")
+    .attr("class", "line")
+    .attr("d", function() {
+        var pathcoordinates = "";
+        for(var key in data){
+            if(pathcoordinates==""){
+                pathcoordinates += ("m"+(Number(x(data[key].month))+Number(35))+","+(y(data[key].revenue)));
+            }
+            else {
+                pathcoordinates += ("L"+(Number(x(data[key].month))+Number(35))+","+y(data[key].revenue));
+            }
         }
-    }
-    
-    var i = 0;
-    
-    for(var key in data) {
-        if(key != i){
-            data[i] = data[key];
-            delete data[key];
-        }
-        i++;
-    }
-    
-    x.domain(data.map(function(d) { if(d==null){return} return d.month; }));
-    y.domain([0, d3.max(data, function(d) { if(d==null){return} return d.revenue; })]);
-
-    svg.append("g")
-        .attr("class", "x axis")
-        .attr("transform", "translate(0," + height + ")")
-        .call(xAxis);
-
-    svg.append("g")
-        .attr("class", "y axis")
-        .call(yAxis)
-        .append("text")
-        .attr("transform", "rotate(-90)")
-        .attr("y", 6)
-        .attr("dy", ".71em")
-        .style("text-anchor", "end")
-        .text("Revenue");
-		
-    svg.append("path")
-		.attr("class", "line")
-		.attr("d", function() {
-			var pathcoordinates = "";
-			for(var key in data){
-				if(pathcoordinates==""){
-					pathcoordinates += ("m"+(Number(x(data[key].month))+Number(35))+","+(y(data[key].revenue)));
-				}
-				else {
-					pathcoordinates += ("L"+(Number(x(data[key].month))+Number(35))+","+y(data[key].revenue));
-				}
-			}
-			return pathcoordinates;
-		});
-        
-    svg.selectAll(".bar")
-        .data(data)
-        .enter().append("rect")
-        .attr("class", "bar")
-        .attr("id", function(d) {if(d==null){return} return d.month} )
-        .attr("x", function(d) {if(d==null){return} return x(d.month); })
-        .attr("width", function(d) {if(d==null){return} return x.rangeBand(); })
-        .attr("y", 0)
-        .attr("height", function(d) {if(d==null){return} return Number(height); })
-        .attr("opacity", "0")
+        return pathcoordinates;
     });
-}
+    
+svg.selectAll(".bar")
+    .data(data)
+    .enter().append("rect")
+    .attr("class", "bar")
+    .attr("id", function(d) {if(d==null){return} return d.month} )
+    .attr("x", function(d) {if(d==null){return} return x(d.month); })
+    .attr("width", function(d) {if(d==null){return} return x.rangeBand(); })
+    .attr("y", 0)
+    .attr("height", function(d) {if(d==null){return} return Number(height); })
+    .attr("opacity", "0")
+});
 
 function type(d) {
   d.revenue = +d.revenue;
