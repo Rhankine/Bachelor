c70fd674f14e1a05cbe3d8a30c2a4927eb45de4f
Bo Carlsen <bocarlsen@hotmail.dk>
2016-05-30 10:46:23 +0200

    No payment site php restructured.

--- NoPayment.php
+++ NoPayment.php
@@ -1,6 +1,6 @@
-<h1>No payment</h1>
-<p>You did not correctly answer the control question and will therefor not be able to get paid.</p>
 <?php
     session_start();
     session_destroy();
 ?>
+<h1>No payment</h1>
+<p>You did not correctly answer the control question and will therefor not be able to get paid.</p>
